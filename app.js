edd0bd81c1d0037b2661437cdd1911790de12889
mash-a <mariya.emv@gmail.com>
2018-04-24 11:10:15 -0400

    minimal css done

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 window.addEventListener('load', event => {
-  console.log("Passive Aggressive App UPPP in da house")
+  console.log("Passive Aggressive App UPPP in da house");
+  const baseURL = 'http://localhost:3004/roommates/';
+  let getWorkBtn = document.querySelector(".getTheWork")
+  getWorkBtn.addEventListener("click", ())
 })
