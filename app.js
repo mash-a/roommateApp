e7a4af5c1fce4122e6c2370abffbcd23e33cdefa
mash-a <mariya.emv@gmail.com>
2018-04-25 17:48:54 -0400

    delete function works

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ window.addEventListener('load', event => {
   const leftSide = document.querySelector(".leftSide");
   const rightSide = document.querySelector(".rightSide");
   const uniqueID = document.querySelector(".iD");
-  let sessionID;
+
   let createTaskTable;
+  let codeEditButton;
 
   const randomNumber = () => {
     let number = Math.random().toString(36).slice(2, 8);
@@ -19,17 +20,23 @@ window.addEventListener('load', event => {
   getWorkBtn.addEventListener("click", (event) => {
     event.preventDefault();
     getWorkBtn.disabled = true;
-    secondPg.setAttribute("id", "second");
-    rightSide.innerHTML = `Create New Task Table<button class="newTaskTableBtn">Create</button> or Enter Code to edit existing Task Table <input id="code" type="integer"><button class="editExistingTableCode">Submit</button>`
+    secondPg.setAttribute("id", "secondPg");
     secondPg.scrollIntoView();
-    const createNewTaskTable = document.querySelector(".newTaskTableBtn");
+    rightSide.innerHTML = `Create New Task Table<button class="newTaskTableBtn">Create</button> or Enter Code to edit existing Task Table <input id="code" type="integer"><button class="editExistingTableCode">Submit</button>`
+    codeEditButton = document.querySelector(".editExistingTableCode");
 
+    on("click", codeEditButton, (event) => {
+      let code = document.querySelector("#code").value;
+      event.preventDefault;
+      editTasks(code);
+      rightSide.innerHTML = "";
+    })
+    const createNewTaskTable = document.querySelector(".newTaskTableBtn");
     createNewTaskTable.addEventListener("click", (event) => {
       event.preventDefault();
-
       const choreForm = document.createElement("form");
       choreForm.innerHTML = `
-        <h6>Create Your Task Table<h6>
+        <h6>Add a task<h6>
         <label>Name</label>
         <br>
         <input type="text" id="personName"/>
@@ -51,43 +58,50 @@ window.addEventListener('load', event => {
         rightSide.innerHTML = "";
         rightSide.appendChild(choreForm);
         leftSide.appendChild(uniqueID);
-        sessionID = randomNumber();
+        const sessionID = randomNumber();
         createTaskTable = document.querySelector("#createTaskTable");
         on("click", createTaskTable, (event) => {
           event.preventDefault();
           createTask(sessionID);
         })
         uniqueID.innerHTML = `Code to access your session again: ${sessionID}`;
-        getAllTasks();
     })
   })
-  console.log(uniqueID.innerHTML);
 
   const getAllTasks = (id) => {
+    let individualID;
     axios.get(baseURL)
       .then (response => {
         listOfTasks.innerHTML = "";
         response.data.forEach(elem => {
          if(elem.unique_id === id){
           let taskItem = document.createElement('li');
-          taskItem.innerHTML = `${elem.task_name} (${elem.due_date})<input id="checkBox" type="checkbox">`;
+          taskItem.innerHTML = `${elem.task_name} (${elem.due_date})<input type="radio" name="task" value="completedTask">`;
+          taskItem.addEventListener("click", (event)=>{
+            let radioBtn = event.target.firstElementChild;
+            radioBtn.checked = true;
+            individualID = elem.id
+            console.log(individualID)
+          })
           listOfTasks.appendChild(taskItem);
           }
-          let finishedButton = document.createElement("button");
-          finishedButton.innerHTML = "Finished a task?";
-          listOfTasks.appendChild(finishedButton);
+        })
+        let finishedButton = document.createElement("button");
+        finishedButton.innerHTML = `<class="delete">Finished a task?`;
+        listOfTasks.appendChild(finishedButton);
+        on("click", finishedButton, () => {
+          deleteTasks(individualID, id);
         })
       })
       .catch(error => {console.error(error);});
   }
 
  const createTask = (id) => {
-    console.log("is this being called?")
     const name = document.getElementById("personName").value;
     const task_name = document.getElementById("taskName").value;
     const frequency = document.getElementById("frequency").value;
     const due_date = document.getElementById("date").value;
-    let unique_id = id;
+    const unique_id = id;
     axios.post(`${baseURL}`, {name, task_name, frequency, due_date, unique_id})
       .then(response => {
         getAllTasks(unique_id);
@@ -99,12 +113,25 @@ window.addEventListener('load', event => {
       .catch(error => {console.error(error);});
   }
 
-  const editTasks = () => {
-
+  const editTasks = (id) => {
+    uniqueID.innerHTML = `${id}`;
+    getAllTasks(id);
   }
+  const deleteTasks = (one_id, all_id) => {
+    axios.delete(`${baseURL}${one_id}`)
+      .then(result => {
+        listOfTasks.innerHTML = "";
+        getAllTasks(all_id);
+      })
+      .catch(error => {console.error(error);})
+  };
+
+
 
   const on = (evt, item, cb) => {
     item.addEventListener(evt, cb);
   }
+
+
 
 })
